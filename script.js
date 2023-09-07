d21adf86d5069155b687b488db238fbabb9e3b4d
KaushalyaDasanayake <kaushalyadasanayake97@gmail.com>
2023-09-07 20:50:34 +0530

    Js statements

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello")
+
+// JS statement
+let x,y,z;
+x=12,
+y=10,
+z = x+y;
+console.log(z);
+
+//Passing parameters with values (Long hand)
+function getName (firstName, lastName){
+    return `${firstName} ${lastName}`;
+}
+
+const myName = getName('John', 'wick');
+console.log('My name is '+ myName)
+
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+const myFullName = getFullName('John','snow');
+console.log('My full name is ' + myFullName)
